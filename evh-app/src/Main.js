6a79040a1bbac17de9a3286983cf8df146e9875c
sunshinejen <jennifer.villacis@seattlecentral.edu>
2021-04-16 17:14:53 -0700

    basic react app with skeleton template pages
    adding a skeleton app with needed pages runs with no errors

---
+++ evh-app/src/Main.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import News from "./components/News";
+import Resources from "./components/Resources";
+import Services from "./components/Services";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>EVH</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/news">News</NavLink></li>
+            <li><NavLink to="/resources">Resources</NavLink></li>
+            <li><NavLink to="/services">Services</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/news" component={News}/>
+            <Route path="/resources" component={Resources}/>
+            <Route path="/services" component={Services}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
---
+++ evh-app/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p> Cras facilisis urna ornare ex volutpat, et
+        convallis erat elementum. Ut aliquam, ipsum vitae
+        gravida suscipit, metus dui bibendum <a href="https://github.com/sunshinejen/evh/projects/1"> Project </a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
