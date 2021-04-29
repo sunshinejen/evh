14a99495bbd7b6e95250aed7382fab82984da021
sunshinejen <jennifer.villacis@seattlecentral.edu>
2021-04-28 17:28:24 -0700

    about page and home update
    updates

--- evh-app/src/components/aboutpage/About.js
+++ evh-app/src/components/aboutpage/About.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
- 
+import VetCard from './VeterinarianCard';
 class About extends Component {
   render() {
     return (
       <div>
-        <h2>About us</h2>
+        <h2>Our Team</h2>
         <p> Cras facilisis urna ornare ex volutpat, et
         convallis erat elementum. Ut aliquam, ipsum vitae
         gravida suscipit, metus dui bibendum <a href="https://github.com/sunshinejen/evh/projects/1"> Project </a>.
         </p>
+        < VetCard />
       </div>
     );
   }
---
+++ evh-app/src/components/aboutpage/VeterinarianCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Card from 'react-bootstrap/Card'
+ 
+class VetCard extends Component {
+  render() {
+    return (
+      <div>
+        <Card className="bg-dark text-white">
+  <Card.Img src="holder.js/100px270" alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card>
+      </div>
+    );
+  }
+}
+ 
+export default VetCard;
