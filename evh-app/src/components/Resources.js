a54b65e4441d36b22cb3f3b10ed982b3c2ecf967
sunshinejen <jennifer.villacis@seattlecentral.edu>
2021-05-20 17:54:12 -0700

    Forms
    adding prescription and food calculator forms

--- evh-app/src/components/Resources.js
+++ evh-app/src/components/Resources.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import MultiStepForm from "./foodCalculator/FoodCalcForm";
+import PrescriptionForm from "./login/PrescriptionForm";
  
 class Resources extends Component {
   render() {
@@ -8,6 +10,10 @@ class Resources extends Component {
         <p> Cras facilisis urna ornare ex volutpat, et
         convallis erat elementum. Ut aliquam, ipsum vitae
         gravida suscipit, metus dui bibendum <a href="https://github.com/sunshinejen/evh/projects/1"> Project </a>.
+        </p>
+        <PrescriptionForm />
+        <p>
+          <MultiStepForm/>
         </p>
       </div>
     );
---
+++ evh-app/src/components/login/PrescriptionForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import axios from "axios";
+class PrescriptionForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      status: "Submit"
+    };   
+  }
+  //this posts to local and not the database yett...
+  handleSubmit(event) {
+    event.preventDefault();  
+    this.setState({ status: "Sending" });  
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/contact",
+      data: this.state,
+    }).then((response) => {
+      if (response.data.status === "sent") {
+        alert("Message Sent");
+        this.setState({ name: "", email: "", message: "", status: "Submit" });
+      } else if (response.data.status === "failed") {
+        alert("Message Failed");
+      }
+    });
+  }
+
+  render() {
+    let buttonText = this.state.status;
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+        <div>
+          <label htmlFor="name"> Pet Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={this.state.name}
+            //onChange={this.handleChange.bind(this)}
+            required
+          />
+            </div>
+            <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="phone"
+            id="phone"
+            value={this.state.email}
+            //onChange={this.handleChange.bind(this)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={this.state.email}
+            //onChange={this.handleChange.bind(this)}
+            required
+          />
+            </div>
+            
+            <div>
+            <label htmlFor="medication">Medication:
+          <select value={this.state.value}>
+            <option value="med1">Medication 1</option>
+            <option value="med2">Medication 2</option>
+            <option value="med3">Medication 3</option>
+            <option value="med4">Medication 4</option>
+          </select>
+            </label>
+            </div>
+        <div>
+          <label htmlFor="message">Notes:</label>
+          <textarea
+            id="message"
+            value={this.state.message}
+            //onChange={this.handleChange.bind(this)}
+            required
+          />
+        </div>
+        <button type="submit">{buttonText}</button>
+      </form>
+    );
+  }
+}
+ 
+export default PrescriptionForm;
